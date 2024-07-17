515625217122918bcbc4496c825c1b522cdd7e8e
Empire of Destruction <elkacrazy8698@gmail.com>
2024-07-17 16:52:56 +0300

    improved pfp, removed unused files and also testing111

--- scripts.js
+++ scripts.js
@@ -1,11 +1,23 @@
 
 // i did nut write this script because i really don't understand js (personallu fuck js)
+
+
 $(document).ready(function(){
 
 
     var titles = ['kotlico ⎻⎼⎽⎼⎻','kotlico ⎼⎻⎼⎽⎼','kotlico ⎽⎼⎻⎼⎽','kotlico ⎼⎽⎼⎻⎼',];
 
     var timeInterval = 300; /** interval between each titles **/
+
+    getTime();
+    async function getTime() {
+    const response = await fetch(
+        'https://timeapi.io/api/Time/current/zone?timeZone=Europe/Kyiv'
+    );
+    const data = await response.json();
+    console.log(data)
+    };
+
 
     exec();
 
@@ -20,6 +32,5 @@ $(document).ready(function(){
             }, timeInterval * (k + 1));
         });
     }
-
 
 });
