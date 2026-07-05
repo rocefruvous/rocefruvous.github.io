92a4a996e928b77887abbb78c5eca9ff8e547725
roce <82180370+rocefruvous@users.noreply.github.com>
2026-07-05 17:46:31 +0300

    feat: metadata

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -1,1 +1,7 @@
 export const ICON = "https://github.com/rocefruvous.png?size=256";
+// METADATA truly cute and whimsical //
+export const TITLE = "Roce's Site";
+export const AUTHOR = "Rocefruvous";
+export const DESCRIPTION =
+  "Professional life waster, amateur code enjoyer, music lover, and the CEO of this website.";
+export const THEME_COLOR = "#f0839b";
